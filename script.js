b5786acb0225cb785922cb6931ae537cc9f93ca3
Tharagaraman <83207969+tharagaramanbalaji@users.noreply.github.com>
2024-11-27 19:40:21 +0530

    Adding the Core files
    no proper structure for assets as its a portfolio website

---
+++ script.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("active");
+});
+
+// Close menu when clicking a link
+document.querySelectorAll(".nav-links a").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navLinks.classList.remove("active");
+}));
